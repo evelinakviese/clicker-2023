8fd6a650d3500784788c6465cd7963cd5f23e346
evelinakviese <128698618+evelinakviese@users.noreply.github.com>
2023-03-23 13:10:30 +0200

    files imported

---
+++ script.js
@@ -0,0 +1,33 @@
+let punkti = 0;
+
+function OnbuttonClick(){
+    punkti += 1;
+    document.getElementById('score').innerHTML = punkti;
+}
+let widh = 200;
+let hight = 200;
+
+widh -= 5;
+height -= 5;
+
+diameter -=5;
+if(diameter  <=5){
+    document.getElementById("score"), innerHTML = "You won!"
+}
+
+document.getElementById("buttom").style.width=widh.toString() + 'px';
+document.getElementById("buttom").style.hight=hight.toString() + 'px';
+
+function getRandomInt (max){
+    return Math.floor(Math.random() *)
+}
+
+function GenereNejausoSkaitli (lidz){
+    return Math.floor
+}
+
+// UZDEVUMS 1
+// Kad klikšķina uz pogu
+// lai poga maina savu krāsu nejaušā veidā
+// lai ir random krāsa
+
